0966da924f9b146b23ef80f33ff885feaef5ab3e
eetann <hideharu092@gmail.com>
2021-02-08 14:31:04 +0900

    ⚙minimize設定を変更 #01

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist/'),
   },
   optimization: {
-    minimize: false,
     splitChunks: {
       name: 'chunk',
       chunks: 'initial',
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,25 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CopyPlugin = require("copy-webpack-plugin");
 const ZipPlugin = require('zip-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
   entry: {
     content: "./src/content-scripts/main.js",
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: false,
+        terserOptions: {
+          output: {
+            ascii_only: false,
+          },
+        },
+      }),
+    ],
   },
   plugins: [
     new ZipPlugin({
