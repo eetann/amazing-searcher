29f9c80868dcae59504e80f769ccedc68e4d987b
eetann <hideharu092@gmail.com>
2021-02-10 19:03:27 +0900

    ♻️順序の変更とコメントの追加#9

--- src/content-scripts/main.js
+++ src/content-scripts/main.js
@@ -2,22 +2,37 @@ import {createApp} from 'vue';
 import App from './App.vue';
 import '../index.css';
 
-let dest = document.getElementById('rcnt');
-let forCoord = document.getElementById('center_col');
-let style = window.getComputedStyle(forCoord);
-let amzSchRootMx = 10;
-let width = parseInt(style.marginLeft) + parseInt(style.width);
+// make dummy div for position adjustment
 let amzSchDummy = document.createElement('div');
 amzSchDummy.id = 'amzSchDummy';
+// set height to prevent element that id=main from shifting
 amzSchDummy.style.height = 0;
-let searchForm = document.getElementById('searchform');
-style = window.getComputedStyle(searchForm);
+
+// set zIndex to display space for this extension deeper than element that id=searchform
+let searchform = document.getElementById('searchform');
+let style = window.getComputedStyle(searchform);
 amzSchDummy.style.zIndex = style.zIndex - 1;
+
+// make space for this extension
 let amzSchRoot = document.createElement('div');
 amzSchRoot.id = 'amzSchRoot';
+
+// calculate value to set marginLeft
+let forCoord = document.getElementById('center_col');
+style = window.getComputedStyle(forCoord);
+let width = parseInt(style.marginLeft) + parseInt(style.width);
+
+// set margin
+let amzSchRootMx = 50;
 amzSchRoot.style.position = 'relative';
 amzSchRoot.style.marginLeft = `${width + amzSchRootMx}px`;
+amzSchRoot.style.marginRight = `${amzSchRootMx}px`;
+
+// set padding
+let amzSchRootPx = 10;
+amzSchRoot.style.paddingLeft = `${amzSchRootPx}px`;
+amzSchRoot.style.paddingRight = `${amzSchRootPx}px`;
 
 amzSchDummy.appendChild(amzSchRoot);
-dest.insertAdjacentElement('afterbegin', amzSchDummy);
+document.getElementById('rcnt').insertAdjacentElement('afterbegin', amzSchDummy);
 createApp(App).mount('#amzSchRoot');
