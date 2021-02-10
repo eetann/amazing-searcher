d79d6e04777b136593bace6ee5ed9cc57c249fbe
eetann <hideharu092@gmail.com>
2021-02-10 17:00:53 +0900

    🎨rhsに依存しない表示に変更#9

--- src/content-scripts/main.js
+++ src/content-scripts/main.js
@@ -1,17 +1,23 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import '../index.css'
+import {createApp} from 'vue';
+import App from './App.vue';
+import '../index.css';
 
 let dest = document.getElementById('rcnt');
-let forCoord = document.getElementById('center_col')
-let style = window.getComputedStyle(forCoord)
-let amzSchRootMx = 10
-let width = parseInt(style.marginLeft) + parseInt(style.width)
-let amzSchRoot = document.createElement('div')
-amzSchRoot.id = 'amzSchRoot'
-amzSchRoot.style.position = 'relative'
-amzSchRoot.style.marginLeft = `${width + amzSchRootMx}px`
-amzSchRoot.style.zIndex = '999'
+let forCoord = document.getElementById('center_col');
+let style = window.getComputedStyle(forCoord);
+let amzSchRootMx = 10;
+let width = parseInt(style.marginLeft) + parseInt(style.width);
+let amzSchDummy = document.createElement('div');
+amzSchDummy.id = 'amzSchDummy';
+amzSchDummy.style.height = 0;
+let searchForm = document.getElementById('searchform');
+style = window.getComputedStyle(searchForm);
+amzSchDummy.style.zIndex = style.zIndex - 1;
+let amzSchRoot = document.createElement('div');
+amzSchRoot.id = 'amzSchRoot';
+amzSchRoot.style.position = 'relative';
+amzSchRoot.style.marginLeft = `${width + amzSchRootMx}px`;
 
-dest.insertAdjacentElement('afterbegin', amzSchRoot)
-createApp(App).mount('#amzSchRoot')
+amzSchDummy.appendChild(amzSchRoot);
+dest.insertAdjacentElement('afterbegin', amzSchDummy);
+createApp(App).mount('#amzSchRoot');
