78ee1727265b0ae407a5b90d69b4dd7363b33a26
eetann <hideharu092@gmail.com>
2021-02-12 14:43:19 +0900

    ✨リンクを追加#5

--- src/components/MyHeading.vue
+++ src/components/MyHeading.vue
@@ -3,7 +3,11 @@
     <div
       class="w-5 h-5 bg-pink-900 bg-opacity-50 border-2 rounded-lg border-gray-700"
     ></div>
-    <p class="text-xl">{{ heading.name }}</p>
+    <a
+      :href="heading.link"
+      class="text-xl text-blue-600 visited:text-purple-600"
+      >{{ heading.name }}</a
+    >
   </div>
 </template>
 
