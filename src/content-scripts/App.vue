e8fc115930b6366b160d932c11241cb90bb75cfd
eetann <hideharu092@gmail.com>
2021-02-12 11:06:13 +0900

    🎨データの幅調整#5

--- src/content-scripts/App.vue
+++ src/content-scripts/App.vue
@@ -1,8 +1,32 @@
 <template>
-  <div class="text-green-600">Mooooe</div>
-  <div>Gooooe</div>
-  <b>mini</b>
-  <div>Fooooe</div>
+  <div class="flex flex-col border rounded-lg border-black">
+    <div class="flex items-center">
+      <div
+        class="w-5 h-5 bg-pink-900 bg-opacity-50 border-2 rounded-lg border-gray-700"
+      ></div>
+      <p class="text-2xl">hoge.com</p>
+    </div>
+    <div class="flex items-center">
+      <div
+        class="w-5 h-5 bg-pink-900 bg-opacity-50 border-2 rounded-lg border-gray-700"
+      ></div>
+      <p class="text-2xl">Document</p>
+    </div>
+    <div class="flex items-center">
+      <div
+        class="w-5 h-5 bg-pink-900 bg-opacity-50 border-2 rounded-lg border-gray-700"
+      ></div>
+      <p class="text-2xl">Search By Document</p>
+    </div>
+    <div class="flex flex-col justify-center">
+      <div class="flex items-center">
+        <div
+          class="w-5 h-5 bg-pink-900 bg-opacity-50 border-2 rounded-lg border-gray-700"
+        ></div>
+        <p class="text-2xl">Search By Google</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
