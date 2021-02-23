0ffaaf752667d89e8a990d5838c020cd0a692b38
eetann <hideharu092@gmail.com>
2021-02-23 16:40:40 +0900

    ✨多言語対応#17

--- src/content-scripts/App.vue
+++ src/content-scripts/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="flex flex-col border rounded-lg border-gray-300">
-    <div class="m-4">
-      <div v-for="item in headings" :key="item.id">
-        <MyHeading :heading="item"></MyHeading>
+    <div
+      class="m-4 space-y-2"
+      v-for="headings in officialInfos"
+      :key="headings.id"
+    >
+      <div v-for="heading in headings.item" :key="heading.id">
+        <MyHeading :heading="heading"></MyHeading>
       </div>
     </div>
   </div>
@@ -10,52 +14,13 @@
 
 <script>
 import MyHeading from "@/components/MyHeading.vue";
-import { getOfficialInfo } from "@/composables/getOfficialInfo.js";
+import { officialInfos } from "@/composables/getOfficialInfo.js";
 export default {
   components: {
     MyHeading,
   },
   setup() {
-    let headings = [
-      {
-        id: 0,
-        title: "hoge.com",
-        link: "https://example.com/",
-        icon: "badge-check",
-      },
-      {
-        id: 1,
-        title: "Document",
-        link: "https://example.com/",
-        icon: "document-text",
-      },
-      {
-        id: 2,
-        title: "Search By Document",
-        link: "https://example.com/",
-        icon: "document-search",
-      },
-      {
-        id: 3,
-        title: "Search By Google",
-        link: "https://example.com/",
-        icon: "search",
-      },
-    ];
-    let info = getOfficialInfo();
-    if (info.length !== 0) {
-      headings[0].title = info[0].home_url
-        .replace(/https?:\/\//, "")
-        .replace(/\/$/, "");
-      headings[0].link = info[0].home_url;
-
-      headings[1].link = info[0].doc_url;
-      headings[2].link = info[0].doc_search;
-      // headings[3].link = info[3].keyword;
-    } else {
-      headings = [];
-    }
-    return { headings };
+    return { officialInfos };
   },
 };
 </script>
