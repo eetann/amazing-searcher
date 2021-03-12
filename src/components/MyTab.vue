bc04cc6a1673412d69594f897e998b674ee8a0d4
eetann <hideharu092@gmail.com>
2021-03-12 16:43:59 +0900

    ✨タブの表示#27

---
+++ src/components/MyTab.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    @click="onClick"
+    class="w-36 text-sm text-center focus:outline-none rounded-t"
+    :class="isActive"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  props: {
+    id: Number,
+    label: String,
+    modelValue: Number,
+  },
+  setup(props, { emit }) {
+    const isActive = computed(() => {
+      console.log(props.modelValue, props.id);
+      return props.modelValue == props.id
+        ? "text-black border-gray-600 border-b-2"
+        : "text-gray-500 hover:bg-gray-100 hover:text-gray-700";
+    });
+    const onClick = () => {
+      emit("update:modelValue", props.id);
+    };
+    return { isActive, onClick };
+  },
+};
+</script>
--- src/options/App.vue
+++ src/options/App.vue
@@ -1,5 +1,28 @@
-<template>This is options page.</template>
+<template>
+  <div class="text-base text-black">Amazing Searcher Setting Page</div>
+  <MyTab
+    v-for="tab in tabs"
+    :key="tab.id"
+    v-bind="tab"
+    v-model="currentId"
+  ></MyTab>
+</template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import MyTab from "@/components/MyTab.vue";
+export default {
+  components: {
+    MyTab,
+  },
+  setup() {
+    const currentId = ref(0);
+    const tabs = ref([
+      { id: 0, label: "Recipe lists" },
+      { id: 1, label: "Custom recipes" },
+      { id: 2, label: "Toggle recipes" },
+    ]);
+    return { currentId, tabs };
+  },
+};
 </script>
