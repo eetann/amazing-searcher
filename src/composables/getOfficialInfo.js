517afc89dcc551308de2e8d674b16e3f909696ca
eetann <hideharu092@gmail.com>
2021-03-22 15:51:45 +0900

    ♻️リファクタリング#24

--- src/composables/getOfficialInfo.js
+++ src/composables/getOfficialInfo.js
@@ -1,41 +1,39 @@
-// import officialInfo from '@/assets/official-info.json';
-
 async function getAmzSchRecipeList() {
-  let recipes = [];
+  let recipeList = [];
   const response = await fetch('https://raw.githubusercontent.com/eetann/amazing-searcher-recipes/main/amz-sch-recipes.json')
   if (response.ok) {
-    recipes = await response.json();
+    recipeList = await response.json();
   }
-  return recipes;
+  return recipeList;
 }
 
 async function getRecipeByList() {
   var queryString = window.location.search;
   let params = new URLSearchParams(queryString);
   let param_q = params.get("q");
-  let recipes = await getAmzSchRecipeList();
-  let infos = [];
-  for (let item of recipes.items) {
+  let recipeList = await getAmzSchRecipeList();
+  let recipes = [];
+  for (let item of recipeList.items) {
     const regex_q = new RegExp('(^|\\s)(' + item.keyword + ')(\\s|$)', 'i');
     if (regex_q.test(param_q)) {
       const regex_r = new RegExp(item.keyword, 'i');
       item.keyword = param_q.replace(regex_r, "").trim();
-      infos.push(item);
+      recipes.push(item);
     }
   }
-  return infos;
+  return recipes;
 }
 
 export async function getRecipes() {
-  let officials = [];
-  let infos = await getRecipeByList();
-  if (infos.length !== 0) {
-    for (let info of infos) {
+  let headings = [];
+  let recipes = await getRecipeByList();
+  if (recipes.length !== 0) {
+    for (let recipe of recipes) {
       let items = [];
 
       // add Homepage
       let heading = {id: 0, title: "Homepage", links: []};
-      for (let item of info.home_url) {
+      for (let item of recipe.home_url) {
         heading.links.push(item);
       }
       if (heading.links.length > 0) {
@@ -44,7 +42,7 @@ export async function getRecipes() {
 
       // add Document
       heading = {id: 1, title: "Document", links: []};
-      for (let item of info.doc_url) {
+      for (let item of recipe.doc_url) {
         heading.links.push(item);
       }
       if (heading.links.length > 0) {
@@ -53,8 +51,8 @@ export async function getRecipes() {
 
       // add Search By Document
       heading = {id: 2, title: "Search By Document", links: []};
-      for (let item of info.doc_search) {
-        item.url = item.url.replace("{}", info.keyword);
+      for (let item of recipe.doc_search) {
+        item.url = item.url.replace("{}", recipe.keyword);
         heading.links.push(item);
       }
       if (heading.links.length > 0) {
@@ -63,18 +61,18 @@ export async function getRecipes() {
 
       // add Search By Google
       heading = {id: 3, title: "Search By Google", links: []};
-      for (let item of info.doc_url) {
+      for (let item of recipe.doc_url) {
         let doc_url = item.url
           .replace(/https?:\/\//, "")
           .replace(/[^/]*\.(html|php)$/, "")
-        item.url = "https://www.google.com/search?q=site:" + doc_url + " " + info.keyword;
+        item.url = "https://www.google.com/search?q=site:" + doc_url + " " + recipe.keyword;
         heading.links.push(item)
       }
       if (heading.links.length > 0) {
         items.push(heading);
       }
-      officials.push({"name": info.name, "items": items});
+      headings.push({"name": recipe.name, "items": items});
     }
   }
-  return officials
+  return headings
 }
